21a1d0f42f4e19faf42f6f4d85bc63d47d425923
mywnajsldkf <mywnajsldkf@gmail.com>
2022-05-12 16:50:20 +0900

    초기 환경 설정

---
+++ README.md
@@ -0,0 +1,3 @@
+# java-calculator
+문자열 계산기 미션 저장소
+
---
+++ src/main/java/CalculatorProject.java
@@ -0,0 +1,5 @@
+public class CalculatorProject {
+    public static void main(String[] args){
+        System.out.println("메인 메서드 실행");
+    }
+}
