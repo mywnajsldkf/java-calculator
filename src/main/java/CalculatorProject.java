c2930a0113ad2666f96ca55dd2b21649d7555b43
mywnajsldkf <mywnajsldkf@gmail.com>
2022-05-20 17:12:54 +0900

    feat: 사칙 연산 후, 연산 결과 출력

--- src/main/java/CalculatorProject.java
+++ src/main/java/CalculatorProject.java
@@ -1,51 +1,83 @@
 import com.sun.org.apache.xerces.internal.xs.StringList;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 import static java.lang.Integer.parseInt;
 
 public class CalculatorProject {
+
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         String userInput;
 
-        List<Integer> numbers = new ArrayList<>();
-        List<String> operators = new ArrayList<>();
-
         System.out.println("사용자 값 입력");
 
         userInput = scanner.nextLine();
-        System.out.println("입력 값: "+ userInput);
 
-        String[] equation = userInput.split(" ");
-        System.out.println("문자식 리스트: "+ equation[0]);
-        System.out.println("문자식 리스트: "+ equation[1]);
+        List<String> equation = Arrays.asList(userInput.split(" "));
 
-        for (int i = 0; i < equation.length; i++){
-            System.out.println(equation[i]);
-            if (equation[i].equals("+") || equation[i].equals("-") || equation[i].equals("*") || equation[i].equals("/")){
-                System.out.println("연산자: "+equation[i]);
-                operators.add(equation[i]);
-            }
-            else{
-                numbers.add(parseInt(equation[i]));
+        Integer sum = parseInt(equation.get(0));
+        String operator = "operator";
+
+        System.out.println("최초 값: "+sum);
+
+        for (int i = 1; i < equation.size(); i++){
+            if (isOperator(equation.get(i))==true){
+                operator = equation.get(i);
+            }else {
+                sum = calculate(sum, operator, parseInt(equation.get(i)));
             }
         }
-
-        System.out.println("숫자: "+numbers);
-        System.out.println("연산자: "+operators);
+        System.out.println(sum);
     }
 
-    /*
-    void UserInput(){
-        Scanner scanner = new Scanner(System.in);
-        String userInput;
-
-        userInput = scanner.nextLine();
-        System.out.println("입력 값: "+ userInput);
+    public static boolean isOperator(String string){
+        if(string.equals("+") || string.equals("-") || string.equals("/") || string.equals("*")){
+            return true;
+        }else {
+            return false;
+        }
     }
-     */
+
+    public static int add(int sum, int num){
+        return sum + num;
+    }
+
+    public static int subtract(int sum, int num){
+        return sum - num;
+    }
+
+    public static int multiply(int sum, int num){
+        return sum * num;
+    }
+
+    public static int divide(int sum, int num){
+        if (num == 0){
+            System.out.println("0으로 나눌 수 없습니다.");
+        }
+        else {
+            return sum / num;
+        }
+        return 0;
+    }
+
+    public static int calculate(int sum, String operator, int num){
+        if (operator.equals("+")){
+            return add(sum, num);
+        }
+        if (operator.equals("-")) {
+            return subtract(sum, num);
+        }
+        if (operator.equals("*")){
+            return multiply(sum, num);
+        }
+        if (operator.equals("/")){
+            return divide(sum, num);
+        }
+        else System.out.println("잘못된 연산자입니다.");
+        return 0;
+    }
 }
-
